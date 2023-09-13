3cad0362dc38ac1db928105b56862ded88549bc2
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-09-13 08:02:39 -0600

    data loaded

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,13 +1,31 @@
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({ blog }) => {
+    const { cover, author_img, author, posted_date, reading_time, hashtags, title} = blog;
     return (
         <div>
-            
+            <img className='my-4' src={cover} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex gap-6'>
+                    <div>
+                        <img className='w-16 h-16 rounded-full' src={author_img} alt="" />
+                    </div>
+                    <div>
+                        <p className='text-2xl'> {author}</p>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <p>{reading_time} min read</p>
+                </div>
+            </div>
+            <h2 className='text-3xl font-semibold'>{title}</h2>
+            {
+                hashtags.map((hash, idx) => <span className='ml-2 text-blue-400' key={idx}><a href="">#{hash}</a></span>)
+            }
         </div>
     );
 };
-Blog.propTypes={
-    blog:PropTypes.array.isRequired
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
 }
 export default Blog;
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,17 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([])
     useEffect(() => {
         fetch('blogs.json')
             .then(res => res.json())
-            .then(data=> setBlogs(data))
-    }, [])
-    console.log(blogs)
+            .then(data => setBlogs(data))
+    }, []);
     return (
-        <div className="md: w-2/3">
-            <h2 className="text-3xl font-medium">Blogs</h2>
+        <div>
+            <div className="md: w-2/3">
+                <h2 className="text-3xl font-medium">Blogs:{blogs.length}</h2>
+            </div>
+            <div className="w-2/3">
+                {
+                    blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                }
+            </div>
         </div>
     );
 }
